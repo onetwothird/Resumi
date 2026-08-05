b800661393dd47ff5df19bf8d4eb3b386d5ea460
onetwothird <angelitodecatoriaa@gmail.com>
2026-08-05 20:18:36 +0800

    feat: create resume card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -56,7 +56,6 @@ export default async function DashboardPage() {
           </div>
         </div>
 
-        {/* Resume Grid */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           
           {/* 1. Create New Resume Card */}
