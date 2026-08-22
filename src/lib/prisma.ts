41ce0bf6fd1e0a16313083a1901ea2074a9afab8
onetwothird <angelitodecatoriaa@gmail.com>
2026-08-23 02:22:05 +0800

    chore: setting up prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,13 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
 const prismaClientSingleton = () => {
-  // 1. Create a database connection pool using your environment variable
   const pool = new Pool({ connectionString: process.env.DATABASE_URL });
   
-  // 2. Wrap the pool in Prisma's PostgreSQL adapter
   const adapter = new PrismaPg(pool);
   
-  // 3. Pass the adapter to PrismaClient
   return new PrismaClient({ adapter });
 };
 
